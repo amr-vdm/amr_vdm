e60e9c88fcd96d39bbe4f6d83a27f7e287382be8
phuc2799 <txphuc02071999@gmail.com>
2025-01-16 18:31:57 +0700

    increase velocity to 30% when no load

--- amr_driver/scripts/amr_safety/speed_filter.py
+++ amr_driver/scripts/amr_safety/speed_filter.py
@@ -3,24 +3,36 @@ import rospy
 import dynamic_reconfigure.client as dc
 
 from std_msgs.msg import Float32
+from amr_msgs.msg import SliderSensorStamped
+
+IN = 1
+OUT = 1
 
 class SpeedFilter():
 
     def __init__(self):
         self.max_speed_ = rospy.get_param("~max_speed", 0.7)
-
-        # self.client_velocity = dc.Client("/move_base_node/RotationShimController/DWBController")
-        # self.client_velocity = dc.Client("/move_base_node/RotationShimController/VectorPursuitController")
-        self.client_velocity = dc.Client("/move_base_node/RotationShimController/TebLocalPlannerROS")
-
+        self.velocity_percentage_ = rospy.get_param("~velocity_per", 0.3)
         self.loop_freq_ = 5.0
+        self.velocity_per_ = 0.0
         self.speed_limit_lane_ = self.max_speed_
         self.speed_at_field_ = self.max_speed_
         self.current_speed_ = self.max_speed_
 
+        self.client_velocity = dc.Client("/move_base_node/RotationShimController/TebLocalPlannerROS")
+
         # Subscribers:
         rospy.Subscriber("speed_limit_lane", Float32, self.speed_limit_lane_callback)
         rospy.Subscriber("speed_at_field", Float32, self.speed_at_field_callback)
+        rospy.Subscriber("/amr/slider_sensor_state", SliderSensorStamped, self.slider_sensor_state_callback)
+
+    def slider_sensor_state_callback(self, msg: SliderSensorStamped):
+        if msg.sensor_state.data:
+            if msg.sensor_state.data[0] == IN:
+                self.velocity_per_ = 0.0
+
+            elif msg.sensor_state.data[1] == OUT:
+                self.velocity_per_ = self.velocity_percentage_
 
     def speed_limit_lane_callback(self, msg: Float32):
         self.speed_limit_lane_ = round(msg.data,2)
@@ -29,22 +41,14 @@ class SpeedFilter():
         self.speed_at_field_ = round(msg.data,2)
 
     def update_velocity(self, speed: float):
-        acc_speed = speed * 2 + 0.1
-        acc_theta = speed * 2 + 0.6
-        DWB_vel = {'max_speed_xy': speed,'max_vel_x': speed,'max_vel_theta': 1.2*speed,
-                   'acc_lim_x': acc_speed, 'decel_lim_x': -acc_speed,
-                   'acc_lim_theta': acc_theta,'decel_lim_theta': -acc_theta}
-        # DWB_vel = {'max_speed_xy': speed,'max_vel_x': speed}
-        VPP_vel = {'desired_linear_vel': speed}
         TEB_vel = {'max_vel_x': speed}
-        # self.client_velocity.update_configuration(DWB_vel)
-        # self.client_velocity.update_configuration(VPP_vel)
         self.client_velocity.update_configuration(TEB_vel)
         rospy.loginfo(f"/speed_filter: Current velocity is {speed}m/s.")
 
     def run(self):
         while not rospy.is_shutdown():
-            speed = min(self.speed_limit_lane_, self.speed_at_field_, self.max_speed_)
+            speed = min(self.speed_limit_lane_, self.speed_at_field_, self.max_speed_) \
+                    + (min(self.speed_limit_lane_, self.speed_at_field_, self.max_speed_) * self.velocity_per_ / 100.0)
             if self.current_speed_ != speed:
                 self.current_speed_ = speed
                 self.update_velocity(self.current_speed_)
